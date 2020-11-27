9382a0bc02a953419b67016b7e029dca7598d6e0
DnlSzin <daniel.dsousafraga@hotmail.com>
2020-11-27 18:09:37 +0000

    Progama funcionando com os testes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE O SEU CÓDIGO AQUI.
-
+    print('Hello There!')
 
 if __name__ == '__main__':
     main()
